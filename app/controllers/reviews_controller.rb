b368c8b1ea1156505c0c458ef5fad8cd96d2539e
Paulina Lopez <paulielopez93@gmail.com>
2019-07-10 10:03:40 -0500

    completed the controller and the views for the restaurant with the simple form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,11 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
+    if @review.save
+      redirect_to restaurant_path(params[:restaurant_id])
+    else
+      render :new
+    end
   end
 
   private
@@ -15,5 +19,4 @@ class ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(:content, :rating)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :restaurants
-  resources :reviews
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
